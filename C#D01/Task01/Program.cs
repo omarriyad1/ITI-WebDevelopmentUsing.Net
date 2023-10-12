935a4ceae646b8f06a1074c2abaafc7fd8aaec50
Omar Abdelmoneam Riyad Ibrahim <119341366+omarriyad1@users.noreply.github.com>
2023-10-12 05:42:00 +0300

    Add files via upload

---
+++ C#D01/Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿//namespace Task01
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, Task01!");
+//        }
+//    }
+//}
+
+//C# 9 Top Level Statment   syntax sygar 
+    //Console.WriteLine("Hello, Task01!");
+    //Console.WriteLine("Hello, Task01!");
+    //Console.WriteLine("Hello, Task01!");
+
+
