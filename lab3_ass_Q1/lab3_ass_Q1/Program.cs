0459cad66f9b69083b7b0ada82e72e749e82e65b
Omar Abdelmoneam Riyad Ibrahim <119341366+omarriyad1@users.noreply.github.com>
2023-10-12 05:53:15 +0300

    Add files via upload

---
+++ lab3_ass_Q1/lab3_ass_Q1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter the size of your array: ");
+        int size = int.Parse(Console.ReadLine());
+        int[] items = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine($"Enter item {i+1}: ");
+            items[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"Items:[{string.Join(",", items )}]");
+        
+
+
+
+    }
+}
---
+++ lab3_ass_Q2/lab3_ass_Q2/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    public static void print2Darr(int[,] arr, int rows, int columns)
+    {
+        int numofitems = rows * columns;
+
+        for (int i = 0; i < numofitems; i++)
+        {
+
+            // Find row and column index
+            int row = i / columns;
+            int col = i % columns;
+            Console.Write(arr[row, col] + " ");
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+            // Given matrix mat[][]
+            int[,] mat = { { 1, 2, 3 },
+                    { 4, 5, 6 },
+                    { 7, 8, 9 } };
+            int N = mat.GetLength(0);
+            int M = mat.GetLength(1);
+            print2Darr(mat, N, M);
+    }
+}
